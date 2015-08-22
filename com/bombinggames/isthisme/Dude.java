e4c9bbb51dcd76a90fb62125f062c46d67dec178
BSVogler <e-mail@benediktvogler.de>
2015-08-22 19:02:39 +0200

    you can kill the dude

--- com/bombinggames/isthisme/Dude.java
+++ com/bombinggames/isthisme/Dude.java
@@ -13,18 +13,23 @@ import com.badlogic.gdx.math.Vector2;
  */
 public class Dude {
 	private static final Sound impact = Gdx.audio.newSound(Gdx.files.internal("com/bombinggames/isthisme/sound/impact.wav"));
-	private Sprite sprite = new Sprite(new Texture(Gdx.files.internal("com/bombinggames/isthisme/graphics/dude.png")));
+	private static final Texture dudeTexture = new Texture(Gdx.files.internal("com/bombinggames/isthisme/graphics/dude.png"));
+	private static final Texture corpseTexture = new Texture(Gdx.files.internal("com/bombinggames/isthisme/graphics/splatter.png"));
+	private final Sprite sprite = new Sprite(dudeTexture);
+	private boolean alive = true;
 		
 	public Dude() {
 		sprite.setPosition(1200, (float) (Math.random()*50+50));
 	}
 	
 	/**
-	 * 
+	 *  
 	 * @param dt time in ms
 	 */
 	public void update(float dt){
-		sprite.setX(sprite.getX() - dt/20f);
+		//walk
+		if (alive)
+			sprite.setX(sprite.getX() - dt/20f);
 	}
 
 	public void draw(Batch batch) {
@@ -32,8 +37,9 @@ public class Dude {
 	}
 	
 	public void hit(){
-		sprite = new Sprite(new Texture(Gdx.files.internal("com/bombinggames/isthisme/graphics/splatter.png")));
+		sprite.setTexture(corpseTexture);
 		impact.play();
+		alive = false;
 	}
 	
 	public Vector2 getPosition(){

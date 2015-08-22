fa8b84e776f68c032dc887ac0f6dbd0695c4d3bc
BSVogler <e-mail@benediktvogler.de>
2015-08-22 15:29:39 +0200

    setup

---
+++ com/bombinggames/isthisme/IsThisMe.java
@@ -0,0 +1,16 @@
+package com.bombinggames.isthisme;
+
+/**
+ *
+ * @author Benedikt Vogler
+ */
+public class IsThisMe {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("asd");
+	}
+	
+}

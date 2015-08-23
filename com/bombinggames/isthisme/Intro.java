daec034b294aadfa2d7db640b82842897e24867e
BSVogler <e-mail@benediktvogler.de>
2015-08-23 18:10:32 +0200

    added fly

--- com/bombinggames/isthisme/Intro.java
+++ com/bombinggames/isthisme/Intro.java
@@ -6,8 +6,11 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  *
@@ -15,13 +18,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  */
 public class Intro implements Screen {
 	private final SpriteBatch batch = new SpriteBatch();
-	private Sprite overlay = new Sprite(new Texture("com/bombinggames/isthisme/graphics/introBg.png"));
+	private Sprite bg = new Sprite(new Texture("com/bombinggames/isthisme/graphics/introBg.png"));
 	private final IsThisMe game;
 	private final Sound intro = Gdx.audio.newSound(Gdx.files.internal("com/bombinggames/isthisme/sound/intro.ogg"));
 	private float timer;
+	private final Animation flyAnimation;
+	private Vector2 flyPos = new Vector2(0, 150);
 
 	public Intro(IsThisMe game) {
 		this.game = game;
+		
+		TextureRegion[] anim = new TextureRegion[]{
+			new TextureRegion(new Texture(Gdx.files.internal("com/bombinggames/isthisme/graphics/fly1.png"))),
+			new TextureRegion(new Texture(Gdx.files.internal("com/bombinggames/isthisme/graphics/fly2.png")))
+		};
+		flyAnimation = new Animation(0.25f, anim);
+		flyAnimation.setPlayMode(Animation.PlayMode.LOOP);
 	}
 	
 	
@@ -35,15 +47,19 @@ public class Intro implements Screen {
 		timer+=delta;
 		Gdx.gl20.glClearColor(0, 0, 0, 1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		if (timer > 20)overlay.setColor(1, 1, 1, 20-timer);
+		if (timer > 20)bg.setColor(1, 1, 1, 20-timer);
 		if (timer > 21 || Gdx.input.isKeyPressed(Input.Keys.X) ) {
 			dispose();
 			game.setScreen(new GameScreen(game));
 			return;
 		}
 		
+		flyPos.x = timer*80f;
+		flyPos.y = timer*10f+300;
+		
 		batch.begin();
-		overlay.draw(batch);
+		bg.draw(batch);
+		batch.draw(flyAnimation.getKeyFrame(timer), flyPos.x, flyPos.y);
 		batch.end();
 	}
 
